08948dcc346e6ef932bd46e8f441f3133cc6b08e
Dimaf <vladfilimontsev@yandex.ru>
2022-08-23 01:36:49 +0300

    Fix email-pattern2

--- src/main/java/by/tms/instagram/service/validator/RegistrationValidator.java
+++ src/main/java/by/tms/instagram/service/validator/RegistrationValidator.java
@@ -2,7 +2,7 @@ package by.tms.instagram.service.validator;
 
 public class RegistrationValidator {
     private static final String USERNAME_PATTERN = "[A-Za-z0-9]{3,15}";
-    private static final String EMAIL_PATTERN = "[\\w\\d-]+([\\\\.\\w\\d_-]+)*@[\\w\\d-]+([\\\\.\\w]+)*";
+    private static final String EMAIL_PATTERN = "[\\w\\d-]+([\\\\.\\w\\d_-]+)*@[\\w\\d-]+(\\.([\\w\\d]+){0,})*";
 
     public boolean checkValidation(String nick, String name, String surname, String email) {
         if ((!nick.matches(USERNAME_PATTERN)
