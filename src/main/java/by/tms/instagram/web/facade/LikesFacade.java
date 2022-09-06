4efb04924f1f1c0d26a08d29b28aedc6e8304d9a
a.a.pligina <a.a.pligina@gmail.com>
2022-09-06 20:18:01 +0300

    improve CommentLikeServlet with LikesFacade

---
+++ src/main/java/by/tms/instagram/web/facade/LikesFacade.java
@@ -0,0 +1,31 @@
+package by.tms.instagram.web.facade;
+
+import by.tms.instagram.entity.Comment;
+import by.tms.instagram.entity.Post;
+import by.tms.instagram.entity.User;
+import by.tms.instagram.service.CommentService;
+import by.tms.instagram.service.PostService;
+import by.tms.instagram.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class LikesFacade {
+    private final UserService userService = UserService.getInstance();
+    private final PostService postService = PostService.getInstance();
+    private final CommentService commentService = CommentService.getInstance();
+
+    public Map<String, Object> getObjects(User currentUser, String email, String postDate, String commentDate) {
+        Map<String, Object> objectMap = new HashMap<>();
+        Optional<User> userByEmail = userService.findByNickNameAndEmail("", email);
+        User userInBase = userByEmail.get();
+        Post post = postService.findPost(userInBase, postDate);
+        Comment comment = commentService.findComment(post, commentDate);
+        comment = commentService.likeComment(post, currentUser, comment, commentDate);
+        objectMap.put("post", post);
+        objectMap.put("user", userInBase);
+        objectMap.put("comment", comment);
+        return objectMap;
+    }
+}
